fcbae64b4e8ab24c08c3cf23702d5b6d6814d201
Will Skora <skorasaurus@gmail.com>
2018-07-13 14:26:18 -0400

    copy the gmap over to mapboxmap

---
+++ public/templates/mapboxmap.php
@@ -0,0 +1,31 @@
+<div class="wp-block-advanced-gutenberg-blocks-gmap">
+	<script>
+		function initMap<?php echo $rand; ?>() {
+
+			var coords = { lat: <?php echo $attributes['latitude']; ?>, lng: <?php echo $attributes['longitude']; ?> }
+
+			var map = new google.maps.Map( document.querySelector( '#gutenblock-gmap-<?php echo $rand; ?>' ), {
+				zoom: <?php echo $attributes['zoom']; ?>,
+				center: coords,
+				styles: <?php echo $style; ?>
+			} );
+
+			var marker = new google.maps.Marker( {
+				position: coords,
+				map: map
+			} );
+
+			var infoWindow = new google.maps.InfoWindow( {
+	    	content: "<p><strong><?php echo htmlspecialchars($attributes['name']); ?></strong></p><p><?php echo htmlspecialchars($attributes['address']); ?></p>"
+	  	} )
+
+			marker.addListener('click', function() {
+	    	infoWindow.open( map, marker );
+	  	} )
+		}
+	</script>
+	<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=<?php echo $api_key; ?>&callback=initMap<?php echo $rand; ?>">
+  </script>
+	<div class="wp-block-advanced-gutenberg-blocks-gmap__canvas" id="gutenblock-gmap-<?php echo $rand; ?>" style="height: <?php echo $attributes['height']; ?>px;"></div>
+</div>
---
+++ src/blocks/mapboxmap/index.js
@@ -0,0 +1,85 @@
+import "./style.scss"
+import "./editor.scss"
+
+import Inspector from "./inspect"
+import Gmap from "./gmap"
+
+const { __ } = wp.i18n
+const { registerBlockType } = wp.blocks
+const { Fragment } = wp.element
+
+export default registerBlockType(
+	'advanced-gutenberg-blocks/mapboxmap',
+	{
+	title: __( "Mapbox Map", 'advanced-gutenberg-blocks' ),
+	description: __( "Display a Mapbox map", 'advanced-gutenberg-blocks' ),
+	category: "common",
+	icon: { background: '#D56561', foreground: "#fff", src: "location-alt" },
+	keywords: [
+    __("mapboxmap", 'advanced-gutenberg-blocks' )
+	],
+	attributes: {
+		address: {
+			type: "string",
+			default: "Paris"
+		},
+		name: {
+			type: "string",
+			default: "",
+		},
+		latitude: {
+			type: "float",
+			default: 48.8566,
+		},
+		longitude: {
+			type: "float",
+			default: 2.3522,
+		},
+		zoom: {
+			type: "integer",
+			default: 15,
+		},
+		height: {
+			type: "integer",
+			default: 400
+		},
+		style: {
+			type: "string",
+			default: "default"
+		}
+	},
+	useOnce: true,
+	edit: props => {
+
+		const { attributes, setAttributes, isSelected } = props
+		const { address, name, zoom, height, style } = attributes
+
+		// If API key is not yet provided
+		// if ( typeof advancedGutenbergBlocksGmap.error !== "undefined" ) {
+		// 	return (
+		// 		<p class="AGB-block-message">
+		// 			{__( "⚠️ You need to provide an API key in ", 'advanced-gutenberg-blocks' )}
+		// 			<a
+		// 				target='_blank'
+		// 				href="/wp-admin/admin.php?page=advanced-gutenberg-blocks-installed#advanced-gutenberg-blocks-gmap"
+		// 			>
+		// 				{__( "Blocks > Installed Blocks > Google Map", 'advanced-gutenberg-blocks' )}
+		// 			</a>
+		// 		</p>
+		// 	)
+		// }
+
+		return (
+			<Fragment>
+				<Inspector { ...{ attributes, setAttributes } } />
+
+				<div className="wp-block-advanced-gutenberg-blocks-gmap">
+					<Gmap { ...{ attributes } } />
+				</div>
+			</Fragment>
+		)
+	},
+	save: props => {
+		return null
+	}
+})
